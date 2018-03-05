8b184958e51bb4e01fce0e786e72ec014a65b82d
anurag <anumfp@gmail.com>
2018-03-05 16:52:13 +0530

    working on department page

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,21 @@
+
+class DepartmentsController < ApplicationController
+	
+	def index
+		
+	end
+
+	def cse
+		
+	end
+
+
+	def mechanical
+		
+	end
+
+	def sci_humanity
+		
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'home#index'
 get 'home/developers'
+
+get 'departments/index'
+get 'departments/cse'
+get 'departments/mechanical'
+get 'departments/sci_humanity'
 end
