5d02e302e29dc41d20eb1d68c04c0532d5bf1a2a
anurag <anumfp@gmail.com>
2018-03-11 02:05:09 +0530

    working on UI and more pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,21 @@ class HomeController < ApplicationController
 	def index
 		
 	end
+	def anoutUs
+		
+	end
+	def campusLife
+		
+	end
+	def principalMassege
+		
+	end
+	def photoGallery
+		
+	end
+	def adminstration
+		
+	end
 	def developers
 		
 	end
